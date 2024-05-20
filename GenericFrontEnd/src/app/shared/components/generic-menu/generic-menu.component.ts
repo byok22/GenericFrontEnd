632957d9515a69df7d7739ad36a1e047b8c59d1d
Kevin Torruco <Kevin_Torruco@jabil.com>
2024-05-20 09:52:56 -0600

    [ADD] Generic Menu Component

---
+++ GenericFrontEnd/src/app/shared/components/generic-menu/generic-menu.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { PrimengModule } from '../../modules/primeng.module';
+import { SelectDropdownComponent } from '../select-dropdown/select-dropdown.component';
+import { MultiSelectDropdownComponent } from '../multi-select-dropdown/multi-select-dropdown.component';
+import { GenericMenuInterface } from './interfaces/generic-menu-item.interface';
+
+@Component({
+  selector: 'app-generic-menu',
+  standalone: true,
+  imports: [
+    CommonModule,
+    PrimengModule,
+    SelectDropdownComponent,
+    MultiSelectDropdownComponent
+  ],
+  styleUrl: './generic-menu.component.css',
+  templateUrl: './generic-menu.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GenericMenuComponent implements OnChanges {
+  @Input() menuItems: GenericMenuInterface[] = [];
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['menuItems']) {
+      this.sortMenuItems();
+    }
+  }
+
+  private sortMenuItems() {
+    this.menuItems.sort((a, b) => a.order - b.order);
+  }
+ }
