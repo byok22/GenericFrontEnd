893b72f8d9b7865fbf1954d5298157a4437c4277
Kevin Torruco <Kevin_Torruco@jabil.com>
2024-05-20 09:54:15 -0600

    [ADD] Test of Generic Menu and Generic Table

--- GenericFrontEnd/src/app/core/master-page/master-page.component.ts
+++ GenericFrontEnd/src/app/core/master-page/master-page.component.ts
@@ -1,68 +1,171 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {  Component, OnInit  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TableColumn } from '../../shared/components/generic-table/interfaces/table-column';
-import { TableFilter } from '../../shared/components/generic-table/interfaces/table-filter';
 import { GenericTableComponent } from '../../shared/components/generic-table/generic-table.component';
 import { GenericTableConfig } from '../../shared/components/generic-table/interfaces/generic-table-config';
 import { TestInterface } from '../../shared/interfaces/testInterface.interface';
-import { BasicKpiComponent } from '../../shared/components/basic-kpi/basic-kpi.component';
 import { BasicKpi } from '../../shared/interfaces/basic-kpi.interface';
+import { TableBuilderFactoryService } from '../../shared/components/generic-table/service/table-builder-factory-service.service';
+import { GenericMenuInterface } from '../../shared/components/generic-menu/interfaces/generic-menu-item.interface';
+import { GenericMenuConcreteBuilder } from '../../shared/components/generic-menu/builder/generic-menu-concret-builder';
+import { GenericMenuComponent } from '../../shared/components/generic-menu/generic-menu.component';
+import { SelectOption } from '../../shared/interfaces/select-option.interface';
+import { SelectDropdownComponent } from '../../shared/components/select-dropdown/select-dropdown.component';
 
 @Component({
   selector: 'app-master-page',
   standalone: true,
   imports: [
-    CommonModule,RouterOutlet, GenericTableComponent
+    CommonModule,RouterOutlet, GenericTableComponent, GenericMenuComponent, SelectDropdownComponent
   ],
   templateUrl: './master-page.component.html',
   styleUrl: './master-page.component.css'
 })
-export class MasterPageComponent {
+export class MasterPageComponent implements OnInit  {
 
-  tableData: TestInterface[] = [
-    { id: 1, name: 'John', age: 30 },
-    { id: 2, name: 'Alice', age: 25 },
-    { id: 3, name: 'Bob', age: 35 }
-  ];
-  kpis: BasicKpi[] = [
-    { title :"Test",  total:"10"},
-    { title :"Test2",  total:"102"}
+  /**
+   *
+   */
+  tableConfig!: GenericTableConfig<TestInterface>;
+  menuItems: GenericMenuInterface[] = [];
+  selectDePrueba: SelectOption[]=[
+    { id:'1',
+      text:'Customer 1'
+    },
+    { id:'2',
+      text:'Customer 2'
+    },
+
   ]
-
-  // Obtener los nombres de las columnas
-   columnFields: string[] = Object.keys(this.tableData[0]);
-
-   columnNames: string[] = this.columnFields.map(this.capitalizeFirstLetter);
-
-   // Crear un arreglo de objetos TableColumn
-   /* tableColumns: TableColumn[] = this.columnNames.map((this.columnFields, index:) => ({
-      field: this.columnNames[index],
-      header: this.columnFields
-    }));*/
-
-    tableColumns: TableColumn[] = this.columnFields.map((element, index)=>{
-      
-      const column: TableColumn = {
-        field : this.columnFields[index],
-        header: this.columnNames[index]
-      }
-      return column;
-      
-    }
-
-    );
+  selectedOption: SelectOption = { id:'2',
+      text:'Customer 2'
+    };
+ // columnFields?: string[] ;
+  constructor(private serviceTable: TableBuilderFactoryService) {
   
-  tableConfig: GenericTableConfig<TestInterface> = {
-    data : this.tableData,
-    rowsPerPage : 10,
-    rowsPerPageOptions :[10, 25, 500],
-    pagination: false,
-    columns: this.tableColumns,
-    kpi: this.kpis
-     
-  } 
-     
+    
+  }
+
+  ngOnInit(): void{
+    this.ConfigTable();
+    this.ConfigMenu();
+    
+
+  }
+  //#region Config Table
+  ConfigTable():void{
+    const builder = this.serviceTable.createBuilder<TestInterface>();
+    builder.Reset();
+    builder.SetTitle("Custom Table");
+    builder.SetDataKey("customId");
+    builder.SetData(this.GetData());
+    builder.SetKpis(this.GetKpis());
+    builder.SetPagination(true);
+    builder.SetRowsPerPage(10);
+    builder.SetRowsPerPageOptions([5, 10, 20]);
+    builder.SetColumns(this.getColumns());
+    builder.SetGlobalFilterFields(["name", "age"]);
+    //builder.SetGroupBy("category");
+   // builder.Generate();
+
+    this.tableConfig = builder.Generate();
+
+  }
+  GetData(): TestInterface[] {
+    return [
+      { id: 1, name: 'John', age: 30 },
+      { id: 2, name: 'Alice', age: 25 },
+      { id: 3, name: 'Bob', age: 35 }
+    ];
+  }
+  GetKpis(): BasicKpi[] {
+    return   [
+      { title :"Test",  total:"10"},
+      { title :"Test2",  total:"102"}
+    ];
+  }
+  getColumns(): TableColumn[] {
+    const columnFields = Object.keys(this.GetData()[0]);
+    const columnNames: string[] = columnFields.map(this.capitalizeFirstLetter);
+
+    return columnFields.map((element, index)=>{           
+      return {
+        field : columnFields[index],
+        header: columnNames[index]
+      };    
+  });
+}
+  //#endregion
+  //#region  ConfigMenu
+  ConfigMenu(): void {
+    const builder = new GenericMenuConcreteBuilder();
+    builder.Reset();
+    const building : SelectOption[] = [{ id: '1', text: 'Building 1' }, { id: '2', text: 'Building 2' }];
+    const selectedOption: SelectOption ={ id: '2', text: 'Building 2' };
+
+    builder.SetDropDown({
+      item: { 
+        selectedOption: selectedOption,
+        options: building, onChange: (event:any) => { /* handle change */ } 
+      },
+      labelText: 'Buildings',
+      order: 1,
+      type: 'dropdown'
+    });
+
+    builder.SetDropDown({
+      item: { 
+       selectedOption: { id: '2', text: 'Customer 2' },
+        options: [{ id: '1', text: 'Customer 1' }, { id: '2', text: 'Customer 2' }], onChange: (event:any) => { /* handle change */ } 
+      },
+      labelText: 'Customers',
+      order: 3,
+      type: 'dropdown'
+    });
+
+    builder.SetDropDown({
+      item: { 
+        selectedOption: { id: '1', text: 'Department 1' },
+        options: [{ id: '1', text: 'Department 1' }, { id: '2', text: 'Department 2' }], onChange: (event:any) => { /* handle change */ }
+       },
+      labelText: 'Departments',
+      order: 2,
+      type: 'dropdown'
+    });
+
+    builder.SetCalendar({
+      item: { model: new Date(), onSelect: (event:any) => { /* handle select */ } },
+      labelText: 'Start Date',
+      order: 4,
+      type: 'calendar'
+    });
+
+    builder.SetButton({
+      item: { onClick: () => { /* handle click */ } },
+      labelText: 'Get Production',
+      order: 5,
+      type: 'button'
+    });
+
+    this.menuItems = builder.Generate();
+  }
+  //#endregion
+  
+
+ 
+
+  
+
+
+ 
+ 
+
+  
+
+   
+
+  
    capitalizeFirstLetter(word: string): string {
         if (!word) return word;
         return word[0].toUpperCase() + word.substr(1).toLowerCase();
